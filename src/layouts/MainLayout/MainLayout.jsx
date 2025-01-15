3e6fd4ea9a13d54295e2c3b000cfad9885c5329b
Ankon Chowdhury <ankonchy1@gmail.com>
2025-01-15 18:04:49 +0600

    all configuration & setup completed successfully

---
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,9 @@
+const MainLayout = () => {
+  return (
+    <div className="font-roboto">
+      <h2 className="text-5xl">Hello from quickgig</h2>
+    </div>
+  );
+};
+
+export default MainLayout;
